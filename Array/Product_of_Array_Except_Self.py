da93d133699ae9ccafac1ddbb8e6eb20e311364c
jeycee <jeyceejeyka635@gmail.com>
2025-11-28 14:59:30 +0300

    adds reason why this algo soltion is effecient

--- Array/Product_of_Array_Except_Self.py
+++ Array/Product_of_Array_Except_Self.py
@@ -1,4 +1,4 @@
-# 🟦 Problem: Product of Array Except Self
+# Problem: Product of Array Except Self
 
 # Given an integer array nums, return an array answer such that:
 
@@ -29,6 +29,23 @@ def product_of_array_except_self(arr):
     return answer
 print(product_of_array_except_self([1,2,3,4]))  # Output: [24,12,8,6]
 print(product_of_array_except_self([-1,1,0,-3,3]))  # Output: [0,0,9,0,0]
+    
+    
+#  Why this algorithm is smart
+# ------------------------------------------------
+
+# Instead of multiplying all elements except i inside a loop (which would be O(n²)), this method:
+
+# builds left products in one pass
+
+# builds right products in another pass
+
+# multiplies them together
+
+# uses O(1) extra space (only prefix & suffix variables)
+
+# Total time: O(n)
+# Total extra space: O(1)
     
 
 # Example 1
