1fe307c7fd07af90310d3d01cc5eed2ae41b3740
jeycee <jeyceejeyka635@gmail.com>
2025-12-05 11:38:11 +0300

    adds search in rotated sorted array solution

--- Array/search_in_rotated_sorted_array.py
+++ Array/search_in_rotated_sorted_array.py
@@ -12,6 +12,37 @@
 
 # Your algorithm must run in O(log n) time.
 
+
+def search_in_rotated_sorted_array(nums, target):
+        
+    left, right = nums[0], len(nums) - 1   
+    
+    while left <= right:            
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        # Check if left half is sorted
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            # Right half sorted
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
+
+print(search_in_rotated_sorted_array([4,5,6,7,0,1,2], 0))  
+print(search_in_rotated_sorted_array([4,5,6,7,0,1,2], 3))
+
+
+
 # Example 1
 
 # Input:
