f88989b6aeadbe7f1b63f6a34ad7347ca3312639
jeycee <jeyceejeyka635@gmail.com>
2025-12-07 12:45:49 +0300

    adds 3sum solution A usin one loop and two pointers complexity o power 2 using enumerate method and why use this method its just same function with new method

--- Array/3Sum.py
+++ Array/3Sum.py
@@ -241,7 +241,57 @@ print(three_sum([0,0,0]))
 
     
 
+# Three Sum using enumerate()
+# def threeSum(nums):
+#     nums.sort()
+#     result = []
+    
+#     for i, a in enumerate(nums):
+        
+#         # skip duplicate values
+#         if i > 0 and a == nums[i - 1]:
+#             continue
+        
+#         left = i + 1
+#         right = len(nums) - 1
+        
+#         while left < right:
+#             total = a + nums[left] + nums[right]
+            
+#             if total < 0:
+#                 left += 1
+#             elif total > 0:
+#                 right -= 1
+#             else:
+#                 result.append([a, nums[left], nums[right]])
+                
+#                 # skip duplicates
+#                 while left < right and nums[left] == nums[left + 1]:
+#                     left += 1
+                    
+#                 while left < right and nums[right] == nums[right - 1]:
+#                     right -= 1
+                
+#                 left += 1
+#                 right -= 1
 
+#     return result
+
+# Why enumerate?
+
+# So instead of writing
+
+# num = nums[i]
+
+
+# We get index + value automatically:
+
+# for i, a in enumerate(nums):
+
+
+# i  points to position
+
+# a points to  number at that position
 
 # Example 1
 
