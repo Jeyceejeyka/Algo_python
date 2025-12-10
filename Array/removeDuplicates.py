3f632e41f02c168619d05eef373fccce00dbc765
jeycee <jeyceejeyka635@gmail.com>
2025-12-10 15:10:21 +0300

    adds removeDuplicates prblem statement solution 1

--- Array/removeDuplicates.py
+++ Array/removeDuplicates.py
@@ -2,6 +2,30 @@
 
 # Given a sorted array, modify it so each number appears at MOST twice, return the new length.
 
+def removeDuplicates(nums):
+    result = []
+    
+    left = 0
+    # right = len(nums) - 1
+    
+    
+    while left <= len(nums) - 1:
+        if nums[left] not in result:
+            result.append(nums[left])
+            left += 1
+        else:
+            count = result.count(nums[left])
+            if count < 2:
+                result.append(nums[left])
+            left += 1
+    for i in range(len(result)):
+        nums[i] = result[i] 
+    return len(result)
+
+print(removeDuplicates([1,1,1,2,2,3,3,3,3,4]))
+print(removeDuplicates([0,0,1,1,1,1,2,2,3,3,4,4]))
+    
+    
 # Example Input
 # nums = [1,1,1,2,2,3,3,3,3,4]
 
