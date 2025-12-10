5547e6d17625b0df5ecfcfb94c0d9cd99e295d9e
jeycee <jeyceejeyka635@gmail.com>
2025-12-10 14:22:04 +0300

    adds removeDuplicates prblem statement

---
+++ Array/removeDuplicates.py
@@ -0,0 +1,12 @@
+# Problem: Remove all duplicates in-place but allow each element only twice
+
+# Given a sorted array, modify it so each number appears at MOST twice, return the new length.
+
+# Example Input
+# nums = [1,1,1,2,2,3,3,3,3,4]
+
+# Expected array (in-place)
+# [1,1,2,2,3,3,4]
+
+# Expected return value
+# 7
