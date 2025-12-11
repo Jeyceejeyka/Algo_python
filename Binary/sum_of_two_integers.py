d2e458dcee1de0f814e91b0f6dca39401840ea7f
jeycee <jeyceejeyka635@gmail.com>
2025-12-12 00:17:20 +0300

    adds ./sum_of_two_integers.py problem statement solution

--- Binary/sum_of_two_integers.py
+++ Binary/sum_of_two_integers.py
@@ -4,6 +4,32 @@
 # You are given two binary strings a and b.
 # Return the sum of the two numbers as a binary string.
 
+def addBinary(a: str, b: str) -> str:
+    a_left, b_left = len(a)-1, len(b) -1
+    carry = 0
+    result = []
+    
+    
+    while a_left >= 0 or b_left >= 0 or carry:
+        total = carry
+        
+        
+        if a_left >= 0:
+            total += int(a[a_left])
+            a_left -= 1
+            
+            
+        if b_left >= 0:
+            total += int(b[b_left])
+            b_left -= 1
+        carry = total // 2
+        result.append(str(total % 2))   
+        
+    return ''.join(reversed(result))
+
+print(addBinary("11", "1"))
+print(addBinary("1010", "1011"))
+
 # Example:
 
 # Input:  a = "11", b = "1"
