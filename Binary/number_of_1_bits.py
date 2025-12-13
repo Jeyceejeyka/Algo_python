50beea71b50111f4bc6a513e8a6383aa9511dd4b
jeycee <jeyceejeyka635@gmail.com>
2025-12-13 22:32:41 +0300

    adds Number of 1 Bits (LeetCode #191)

---
+++ Binary/number_of_1_bits.py
@@ -0,0 +1,27 @@
+# Number of 1 Bits (LeetCode #191)
+
+# Problem Statement:
+# Write a function that takes an unsigned integer and returns the number of '1' bits it has.
+# This count is also known as the Hamming weight.
+
+# Example 1
+# Input:  n = 00000000000000000000000000001011
+# Output: 3
+
+
+# Explanation:
+# The input binary string has three '1' bits.
+
+# Example 2
+# Input:  n = 00000000000000000000000010000000
+# Output: 1
+
+# Example 3
+# Input:  n = 11111111111111111111111111111101
+# Output: 31
+
+# Constraints
+
+# The input must be treated as an unsigned integer.
+
+# The input value fits in a 32-bit integer.
