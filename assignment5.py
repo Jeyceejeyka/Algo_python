de240c6769fb9f6aead5cec9bed321836c175384
jeycee <jeyceejeyka635@gmail.com>
2025-12-15 22:12:08 +0300

    adds assignmen5.py

---
+++ assignment5.py
@@ -0,0 +1,9 @@
+# Prompt the user to enter their name
+name = input("Enter your name: ")
+
+# Reverse the string using slicing
+reversed_name = name[::-1]
+
+# Output
+print("Reversed name:", reversed_name)
+
