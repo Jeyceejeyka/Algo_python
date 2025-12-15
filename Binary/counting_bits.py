3ba0c82d45d934c78d682abaa3159ec5f1f5ad37
jeycee <jeyceejeyka635@gmail.com>
2025-12-15 23:23:47 +0300

    adds ./counting_bits.py problem statement solution B

--- Binary/counting_bits.py
+++ Binary/counting_bits.py
@@ -3,14 +3,24 @@
 
 # Given an integer n, return an array ans of length n + 1 such that for each i (0 ≤ i ≤ n), ans[i] is the number of 1’s in the binary representation of i.
 # solution: A simple approach is to iterate through each number from 0 to n, convert each number to its binary representation using the built-in bin() function, and count the number of 1's using the count() method. We store these counts in a list and return it.
-def counting_bits(n: int):
-    ans = []
-    for i in range(n + 1):
-        # Count the number of 1's in the binary representation of i
-        count_ones = bin(i).count('1')
-        ans.append(count_ones)
-    return ans
+# def counting_bits(n: int):
+#     ans = []
+#     for i in range(n + 1):
+#         # Count the number of 1's in the binary representation of i
+#         count_ones = bin(i).count('1')
+#         ans.append(count_ones)
+#     return ans
     
+
+
+#SOLUTION: Bit Manipulation
+# Another efficient approach is to use bit manipulation. The number of 1's in the binary representation of a number can be derived from the number of 1's in the binary representation of the 
+# number obtained by removing the least significant bit (i.e., i >> 1) plus 1 if the least significant bit is 1 (i.e., i & 1).
+def counting_bits(n: int):
+    ans = [0] * (n + 1)
+    for i in range(1, n + 1):
+        ans[i] = ans[i >> 1] + (i & 1)
+    return ans
 
 
 # Example 1
