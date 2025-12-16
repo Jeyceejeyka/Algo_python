1f3e816f14648917f3f9a3c43d447777b19f500b
jeycee <jeyceejeyka635@gmail.com>
2025-12-16 22:39:05 +0300

    adds binary missing number problem statement

---
+++ Binary/missing_number.py
@@ -0,0 +1,60 @@
+# Missing Number
+# Problem Statement
+
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+# Example 1
+
+# Input:
+
+# nums = [3, 0, 1]
+
+
+# Output:
+
+# 2
+
+
+# Explanation:
+# The numbers are in the range [0, 3].
+# The number 2 is missing.
+
+# Example 2
+
+# Input:
+
+# nums = [0, 1]
+
+
+# Output:
+
+# 2
+
+# Example 3
+
+# Input:
+
+# nums = [9,6,4,2,3,5,7,0,1]
+
+
+# Output:
+
+# 8
+
+# Constraints
+
+# n == nums.length
+
+# 1 ≤ n ≤ 10⁴
+
+# 0 ≤ nums[i] ≤ n
+
+# All numbers in nums are unique
+
+# Follow-up
+
+# Could you implement a solution using:
+
+# O(n) time complexity
+
+# O(1) extra space?
