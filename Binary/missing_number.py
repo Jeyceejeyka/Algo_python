82a6fdce637d4b58c3b342b47caf2d4e4a211b1a
jeycee <jeyceejeyka635@gmail.com>
2025-12-17 10:14:45 +0300

    adds binary missing number problem statement solution A

--- Binary/missing_number.py
+++ Binary/missing_number.py
@@ -3,6 +3,20 @@
 
 # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
+# Solution: Arithemtic Sum Formula
+# We can find the missing number by calculating the expected sum of the first n natural numbers using
+def missing_number(nums):
+    n = len(nums)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
+
+
+print(missing_number([3, 0, 1]))  
+print(missing_number([0, 1]))   
+print(missing_number([9,6,4,2,3,5,7,0,1])) 
+
+
 # Example 1
 
 # Input:
